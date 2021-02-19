ab050774ae69d72614b31cd948be21f770d5d3db
george <tang19970214@gmail.com>
2021-02-19 15:57:47 +0800

    upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use('/', serveStatic(path.join(__dirname, '/.nuxt/dist')));
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 app.get(/.*/, function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
